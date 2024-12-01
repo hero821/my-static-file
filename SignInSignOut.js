b264d2eb8cd0b73840a14a927ef592efd92b0ca4
LiQiang <hero88821@qq.com>
2024-12-01 12:10:23 +0800

    添加经纬度

--- SignInSignOut.js
+++ SignInSignOut.js
@@ -2,39 +2,48 @@ if (window.VConsole != undefined) {
     new window.VConsole();
 }
 
-window.addEventListener("error", function (event) {
+window.addEventListener('error', function (event) {
     event.stopImmediatePropagation();
-    console.log(">>> error event stop");
+    console.log('>>> error event stop');
 }, true);
 
+var geoArr = [
+    {name: '石家庄|星际中心', lng: '114.623720', lat: '38.038799'},
+    {name: '北京|庄胜广场', lng: '116.375458', lat: '39.896526'}
+];
+
+var index = prompt('请输入编号：\n' + geoArr.map(function (value, index) {
+    return index + '：' + value.name;
+}).join('\n'));
+
 var getLocation = function (options) {
-    var lng = "";
-    var lat = "";
+    var lng = geoArr[index].lng;
+    var lat = geoArr[index].lat;
 
     var min, max, rand;
 
     min = -0.0011;
     max = 0.0011;
-    rand = Math.random() * (max - min) + min;
+    rand = (Math.random() * (max - min) + min);
     lng = (lng + rand).toFixed(6);
 
     min = -0.0009;
     max = 0.0009;
-    rand = Math.random() * (max - min) + min;
+    rand = (Math.random() * (max - min) + min);
     lat = (lat + rand).toFixed(6);
 
-    console.log(">>> 经纬度：", lng, lat);
+    console.log('>>> 经纬度：', lng, lat);
 
-    options.success.call(this, { longitude: lng, latitude: lat, accuracy: "0.5" });
-};
+    options.success.call(this, {longitude: lng, latitude: lat, accuracy: '0.5'});
+}
 
 setInterval(function () {
     if (window.wx == undefined) {
-        console.log(">>> window.wx is undefined");
+        console.log('>>> window.wx is undefined');
         return;
     }
     if (window.wx.getLocation == getLocation) {
-        console.log(">>> window.wx.getLocation is configured");
+        console.log('>>> window.wx.getLocation is configured');
         return;
     }
     window.wx.getLocation = getLocation;
